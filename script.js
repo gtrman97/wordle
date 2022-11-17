5b9bf7c5e231e26262fb5699b564704a576a8fc4
Andrew Killian <ack97@txstate.edu>
2022-11-16 23:22:49 -0600

    made first async function!

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// let row1Squares = document.querySelector('.row1').childNodes;
-// let guess = "CRANE", i = 0;
+// let row1Squares = document.querySelector('.row1').children;
+// console.log(row1Squares);
+// let word = "CRANE", i = 0;
 // for(let square of row1Squares) {
-//     console.log(row1Squares[i]);
-//     square.innerText = guess[i];
+//     // console.log(row1Squares[i]);
+//     square.innerText = word[i];
 //     i++;
 // }
 
@@ -11,10 +12,53 @@
 //     console.log(e.target);
 // })
 
-window.addEventListener('keyup', (e) => {
-    let square = document.querySelector('.square1');
-    let code = e.code;
-    let len = code.length;
-    square.innerText = code[len-1];
-console.log(typeof e.code);
-})
+// let word = fetch("https://words.dev-apis.com/word-of-the-day")
+//   .then((res) => res.json())
+//   .then((body) => body.word)
+//   .catch((error) => console.error(error));
+// console.log(`word of the day is ${word}`);
+
+async function getWordOfDay(){
+    let response = await fetch('https://words.dev-apis.com/word-of-the-day');
+    let body = await response.json();
+    let word = body.word;
+    console.log(typeof body);
+    // console.log(body);
+    // console.log(word);
+    return word.toUpperCase()
+}
+getWordOfDay();
+
+// let row = 1;
+// let character = 0;
+let guess = {
+  row: 1,
+  character: 0,
+};
+
+window.addEventListener("keyup", (e) => {
+  guessLetter(guess, e);
+  console.log(`row is ${guess.row} and char is ${guess.character}`);
+  if (guess.character === 5) incrementRow();
+  if (guess.row === 7) {
+    setTimeout(() => {
+      endGame();
+    }, 1000);
+  }
+});
+
+function guessLetter(guess, e) {
+  let squares = document.querySelector(`.row${guess.row}`).children;
+  let code = e.code;
+  let len = code.length;
+  squares[guess.character].innerText = code[len - 1];
+  guess.character++;
+}
+
+function incrementRow() {
+  guess.row++;
+  guess.character = 0;
+}
+function endGame() {
+  alert("You Win!");
+}
